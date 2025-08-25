1c463b165ef6810daf489815ce79ee0acdb01336
Dhanushka <dhanushkamcr@gmail.com>
2025-08-24 22:48:23 -0500

    Filter Data (SQL WHERE)

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ new_user = User(name='Alice', email='alice@example.com')
 new_user2 = User(name='DC', email='dc@example.com')
 
 # Add and commit the user to the database
-session.add(new_user)
-session.add(new_user2)
-session.commit()
+# session.add(new_user)
+# session.add(new_user2)
+# session.commit()
 
 
 # Query all users
@@ -29,3 +29,11 @@ users = session.execute(query).scalars().all()
 # Print the users
 for user in users:
     print(user.name, user.email)
+
+
+
+# Query users with a specific email
+query = select(User).where(User.name == "Alice")
+user = session.execute(query).scalars().first()
+
+print(user.name)  # Outputs: Alice

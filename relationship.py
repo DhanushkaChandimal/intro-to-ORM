fd8b2d9b1380623e2cf3c81f86df8655373b34b4
Dhanushka <dhanushkamcr@gmail.com>
2025-08-25 01:20:22 -0500

    Adding and Using Relationships

--- relationship.py
+++ relationship.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, String, ForeignKey
+from sqlalchemy import create_engine, String, ForeignKey, select
 from sqlalchemy.orm import DeclarativeBase, Session, Mapped, mapped_column, relationship
 from typing import List, Optional
 
@@ -34,5 +34,33 @@ class Pet(Base):
     # Many-to-One: Pet -> User
     owner: Mapped["User"] = relationship(back_populates="pets")
 
-# Create tables
-Base.metadata.create_all(engine)
+# # Create tables
+# Base.metadata.create_all(engine)
+
+session = Session(engine)
+
+# new_user = User(name="DC", fullname="DC DC")
+# session.add(new_user)
+# session.commit()
+
+# new_pet = Pet(name="Barry", animal="Hamster", user_id=1)  # Assuming User with id=1 exists
+# session.add(new_pet)
+# session.commit()
+
+
+#First we query for our User, Alicia 
+query = select(User).where(User.name == 'Alice')
+user = session.execute(query).scalars().first()
+
+# Accessing the pets relationship
+for pet in user.pets:
+    print(pet.name)  # Outputs: Barry
+    
+    
+    
+#First we query our pet
+query = select(Pet).where(Pet.name == 'Barry')
+pet = session.execute(query).scalars().first()
+
+# Accessing the owner relationship
+print(pet.owner.name)  # Outputs: Alice
